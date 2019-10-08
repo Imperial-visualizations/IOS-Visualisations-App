e3f67601c35c5b3d1d9c7b54a799003ec31bb755
Vedant Varshney <vv218@ic.ac.uk>
2019-10-08 23:34:04 +0100

    Basic reachability fixed - splash screen logo pulses when no connection detected rather than showing a blank table
    Static launch screen constraints fixed
    
    TODO:
    Add WiFi symbol subview when unreachable - not yet working

--- VisualisationsUIKit/AnimatedLaunchScreenController.swift
+++ VisualisationsUIKit/AnimatedLaunchScreenController.swift
@@ -35,35 +35,96 @@ class AnimatedLaunchScreenController: UIViewController {
         
         reachability = try? Reachability()
         
-        //NOT WORKING - PRIORITY
-//        reachability?.whenReachable = { reachability in
-//            DispatchQueue.main.async {
-//                print("reached")
-//                NetworkManager.SharedInstance.decodeJSON()
-//                SplashView.heartAttack = true
-//            }
+        
+//TEMPORARY - need to remove
+//        if reachability?.connection != .unavailable {
+//            print("reachable")
+//
+//            //Decode JSON - need to exit splash view only when fetch is finished, cannot access SplashView if decoding code is a method in NetworkManager. URLSession is async!
+//
+//            //let url_name = "https://api.myjson.com/bins/of7n9"
+//            let url_name = "https://raw.githubusercontent.com/VedantVarshney/VisualisationsPersonal/master/DataModel"
+//            guard let url = URL(string: url_name) else { fatalError("JSON URL not found") }
+//
+//            URLSession.shared.dataTask(with: url) { (data, _, _) in
+//                guard let data = data else { fatalError("JSON data not loaded") }
+//
+//                do {
+//                    let decodedDataModel = try JSONDecoder().decode(DataModel.self, from: data)
+//                    visualisations = decodedDataModel.Visualisations
+//                    filteredVisualisations = decodedDataModel.Visualisations
+//
+//                    SplashView.heartAttack = true
+//
+//                } catch {
+//                    print(error)
+//                }
+//
+//            }.resume()
 //        }
+//    }
+        
+        let wifiImageView = UIImageView()
+        var wifiImage: UIImage
+        if #available(iOS 13.0, *) {
+            wifiImage = UIImage(systemName: "wifi.slash")!
+//            TODO - optional ?? provide default
+            
+        } else {
+            wifiImage = UIImage(contentsOfFile: "logo_white_solid_NBG")!
+            //TODO - add wifi symbol
+        }
+        wifiImageView.image = wifiImage
+        
+//      change size?
+        wifiImageView.frame = CGRect(origin: CGPoint(x: 0, y: view.frame.size.height * -1/3), size: wifiImage.size)
+        
+        wifiImageView.contentMode = UIView.ContentMode.scaleAspectFit
+        
+        //add constraints to sub view
+        
+        //???
+//        wifiImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+////      horizontally centers
+//        let horizConstr = NSLayoutConstraint(item: wifiImageView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1.0, constant: 0.0)
+//
+//        let vertOffset = view.frame.size.height * 1/3
+//
+////      symbol 2/3 way down from top of screen
+//        let vertConstr = NSLayoutConstraint(item: wifiImageView, attribute: .centerY, relatedBy: .lessThanOrEqual, toItem: view, attribute: .centerY, multiplier: 1.0, constant: vertOffset)
+//
+//        wifiImageView.addConstraints([horizConstr, vertConstr])
         
         
-        //TEMPORARY
-        if reachability?.connection != .unavailable {
+        //TODO - unreachable state needs testing
+        reachability?.whenReachable = { _ in
             print("reachable")
+            
+//          removes wifi symbol when reachable
+            if self.view.subviews.contains(wifiImageView) {
+                wifiImageView.removeFromSuperview()
+            }
 
             //Decode JSON - need to exit splash view only when fetch is finished, cannot access SplashView if decoding code is a method in NetworkManager. URLSession is async!
 
-            //let url_name = "https://api.myjson.com/bins/of7n9"
+//            let url_name = "https://api.myjson.com/bins/of7n9"
             let url_name = "https://raw.githubusercontent.com/VedantVarshney/VisualisationsPersonal/master/DataModel"
             guard let url = URL(string: url_name) else { fatalError("JSON URL not found") }
 
+//            TODO - error handling
             URLSession.shared.dataTask(with: url) { (data, _, _) in
                 guard let data = data else { fatalError("JSON data not loaded") }
 
                 do {
+
                     let decodedDataModel = try JSONDecoder().decode(DataModel.self, from: data)
                     visualisations = decodedDataModel.Visualisations
                     filteredVisualisations = decodedDataModel.Visualisations
 
+                    //signal to exit splash view
                     SplashView.heartAttack = true
+
 
                 } catch {
                     print(error)
@@ -72,44 +133,18 @@ class AnimatedLaunchScreenController: UIViewController {
             }.resume()
         }
         
-        //SplashView.heartAttack = true
-    }
+        reachability?.whenUnreachable = { _ in
+            print("unreachable")
+            self.view.addSubview(wifiImageView)
+        }
         
-        //NOT WORKING?!
-//        reachability?.whenReachable = { _ in
-//            print("reachable")
-//
-//            //Decode JSON - need to exit splash view only when fetch is finished, cannot access SplashView if decoding code is a method in NetworkManager. URLSession is async!
-//
-//            let url_name = "https://api.myjson.com/bins/of7n9"
-//            //let url_name = "https://raw.githubusercontent.com/VedantVarshney/VisualisationsPersonal/master/DataModel"
-//            guard let url = URL(string: url_name) else { fatalError("JSON URL not found") }
-//
-//            URLSession.shared.dataTask(with: url) { (data, _, _) in
-//                guard let data = data else { fatalError("JSON data not loaded") }
-//
-//                do {
-//
-//                    let decodedDataModel = try JSONDecoder().decode(DataModel.self, from: data)
-//                    visualisations = decodedDataModel.Visualisations
-//                    filteredVisualisations = decodedDataModel.Visualisations
-//
-//                    //signal to exit splash view
-//                    SplashView.heartAttack = true
-//
-//
-//                } catch {
-//                    print(error)
-//                }
-//
-//            }.resume()
-//        }
-//
-//        print("done")
-//
-//        //UIApplication.shared.keyWindow?.rootViewController = navVC
-//
-//    }
+        do {
+            try reachability?.startNotifier()
+        } catch {
+            print("Unable to start notifier")
+        }
+
+    }
     
 }
 
